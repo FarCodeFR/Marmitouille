063b3bedb30c09682844deaf6a2aecb91e698d3b
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-29 13:35:09 +0100

    Remove elements who disfunctioned

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ const recipes = [
     likes: 0,
   },
 ];
-import Recette from "./Components/recette";
+// import Recette from "./Components/recette";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -117,7 +117,6 @@ function App() {
       <nav className="maNavBar">
         <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       </nav>
-      <Recette />
       <h2 className="titre">Trouver les meilleurs recettes</h2>
       <main>
         {filteredRecipes.map((el) => {
